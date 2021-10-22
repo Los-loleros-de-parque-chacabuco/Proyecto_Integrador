8ebda822c661d7250e2e811c15e3199971f3aacf
Christiang98 <chrisu9855@gmail.com>
2021-10-22 03:47:45 -0300

    Trabajo terminado

--- app.js
+++ app.js
@@ -54,6 +54,12 @@ iconoCarrito.onclick = () => {
     }
     console.log(carrito)
 }
+let progress = document.getElementById('progressbar');
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function (){
+    let progressHeight = (window.pageYOffset / totalHeight) * 100;
+    progress.style.height = progressHeight + "%"
+}
 
 
 
